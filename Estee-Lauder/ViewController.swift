176f9e71c507a6d9094d292ec055cef8ca2639ae
kerochan37 <minakimble@gmail.com>
2021-07-22 11:37:03 -0500

    Initial Commit

---
+++ Estee-Lauder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Estee-Lauder
+//
+//  Created by Mina Kimble on 7/22/21.
+//  Copyright © 2021 Mina Kimble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
