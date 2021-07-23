c2ea1f543796cd629faea283cba36eea85775b7f
kerochan37 <minakimble@gmail.com>
2021-07-22 22:13:42 -0500

    Final Estee Lauder Challenge

--- Estee-Lauder/ViewController.swift
+++ Estee-Lauder/ViewController.swift
@@ -10,9 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lightcool: UILabel!
+    @IBOutlet weak var lightneutral: UILabel!
+    @IBOutlet weak var lightwarm: UILabel!
+    @IBOutlet weak var mediumcool: UILabel!
+    @IBOutlet weak var mediumneutral: UILabel!
+    @IBOutlet weak var mediumwarm: UILabel!
+    @IBOutlet weak var deepcool: UILabel!
+    @IBOutlet weak var deepneutral: UILabel!
+    @IBOutlet weak var deepwarm: UILabel!
+    
+
+    
+    @IBAction func lightButton(_ sender: Any) {
+        lightcool.isHidden = false
+        lightneutral.isHidden = false
+        lightwarm.isHidden = false
+        
+    }
+    @IBAction func mediumButton(_ sender: Any) {
+        mediumcool.isHidden = false
+        mediumneutral.isHidden = false
+        mediumwarm.isHidden = false
+    }
+    
+    @IBAction func darkButton(_ sender: Any) {
+        deepcool.isHidden = false
+        deepneutral.isHidden = false
+        deepwarm.isHidden = false
+    }
+    
+    
+    
+    @IBAction func coolButton(_ sender: Any) {
+        lightneutral.isHidden = true
+        lightwarm.isHidden = true
+        mediumneutral.isHidden = true
+        mediumwarm.isHidden = true
+        deepneutral.isHidden = true
+        deepwarm.isHidden = true
+    }
+    
+    @IBAction func neutralButton(_ sender: Any) {
+        lightcool.isHidden = true
+        lightwarm.isHidden = true
+        mediumcool.isHidden = true
+        mediumwarm.isHidden = true
+        deepcool.isHidden = true
+        deepwarm.isHidden = true
+    }
+    @IBAction func warmButton(_ sender: Any) {
+        lightneutral.isHidden = true
+        lightwarm.isHidden = true
+        mediumneutral.isHidden = true
+        mediumwarm.isHidden = true
+        deepneutral.isHidden = true
+        deepwarm.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lightcool.isHidden = true
+        lightneutral.isHidden = true
+        lightwarm.isHidden = true
+        mediumcool.isHidden = true
+        mediumneutral.isHidden = true
+        mediumwarm.isHidden = true
+        deepcool.isHidden = true
+        deepneutral.isHidden = true
+        deepwarm.isHidden = true
     }
 
 
